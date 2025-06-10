0aff5c043b6899fa4c9ceb307660a8589e6c9149
toni-heittola <toni.heittola@gmail.com>
2025-06-10 12:49:22 +0300

    Fix missing key error with content.metadata

--- bnews.py
+++ bnews.py
@@ -547,10 +547,14 @@ def bnews(content):
             content.metadata[u'styles'] = []
 
         for element in html_elements['js_include']:
+            if 'scripts' not in content.metadata:
+                content.metadata[u'scripts'] = []
             if element not in content.metadata[u'scripts']:
                 content.metadata[u'scripts'].append(element)
 
         for element in html_elements['css_include']:
+            if 'styles' not in content.metadata:
+                content.metadata[u'styles'] = []
             if element not in content.metadata[u'styles']:
                 content.metadata[u'styles'].append(element)
 
